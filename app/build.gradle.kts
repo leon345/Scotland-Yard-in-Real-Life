9f39d1a81a0eea0ddfb84572af08afd8a715a53b
jschophu <jannes.schophuis@hs-osnabrueck.de>
2026-01-14 22:17:12 +0100

    added mapsforge dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.database)
+    implementation("com.github.mapsforge.mapsforge:mapsforge-map-android:0.27.0@jar")
+    implementation("com.caverock:androidsvg:1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
