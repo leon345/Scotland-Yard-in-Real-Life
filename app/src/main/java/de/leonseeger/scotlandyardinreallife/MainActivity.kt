a4cb7b98f2342a3b958ace5f3d1d32ff52c85698
jschophu <jannes.schophuis@hs-osnabrueck.de>
2026-01-22 03:57:44 +0100

    able to define polygons with clicks on map

--- app/src/main/java/de/leonseeger/scotlandyardinreallife/MainActivity.kt
+++ app/src/main/java/de/leonseeger/scotlandyardinreallife/MainActivity.kt
@@ -1,7 +1,9 @@
 package de.leonseeger.scotlandyardinreallife
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,32 +17,40 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import de.leonseeger.scotlandyardinreallife.ui.components.CustomMap
 import androidx.compose.ui.unit.dp
 import de.leonseeger.scotlandyardinreallife.game.boundary.CreateGameActivity
 import de.leonseeger.scotlandyardinreallife.ui.component.PrimaryButton
+import de.leonseeger.scotlandyardinreallife.ui.components.PlayMap
 import de.leonseeger.scotlandyardinreallife.ui.theme.ScotlandYardInRealLifeTheme
 import org.maplibre.android.MapLibre
+import org.maplibre.android.geometry.LatLng
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val mapId = "landscape-v4"
-        val styleUrl = "https://tiles.openfreemap.org/styles/liberty"
         MapLibre.getInstance(this)
-
         enableEdgeToEdge()
         setContent {
+            val playMap = remember { PlayMap() }
+
+
+
+
             ScotlandYardInRealLifeTheme {
-                CustomMap(modifier = Modifier.fillMaxSize(),
-                    styleUrl = styleUrl, lat = 52.2720, lon = 8.0482,
-                    appContext = this
+                PlayScreen(
+                    playMap = playMap,
+                    context = LocalContext.current
                 )
+                /*CustomMap(modifier = Modifier.fillMaxSize(),
+                    lat = 52.2720, lon = 8.0482,
+                    appContext = this
+                )*/
                 /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MainScreen(
                         modifier = Modifier.padding(innerPadding)
@@ -49,6 +59,34 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun PlayScreen(
+    playMap: PlayMap,
+    context: Context
+) {
+    playMap.CustomMap(
+        modifier = Modifier.fillMaxSize(),
+        lat = 52.2720,
+        lon = 8.0482,
+        appContext = context,
+        onMapReady = { map ->
+            /*var positions = arrayOf(
+                LatLng(52.267, 8.0532),
+                LatLng(52.272, 8.0575),
+                LatLng(52.281, 8.0432),
+                LatLng(52.273, 8.0402)
+            )*/
+            //map.addPolyToMap(positions);
+            map.getMapLibreMap().addOnMapClickListener { point ->
+                if(!map.addPolyPoint(point)){
+                    Toast.makeText(context, "Polygon braucht mehr Punkte", Toast.LENGTH_SHORT).show()
+                }
+                true
+            }
+        }
+    )
 }
 
 @Composable
--- app/src/main/java/de/leonseeger/scotlandyardinreallife/ui/components/Map.kt
+++ app/src/main/java/de/leonseeger/scotlandyardinreallife/ui/components/Map.kt
@@ -10,69 +10,132 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import  android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.runtime.mutableStateOf
 import de.leonseeger.scotlandyardinreallife.BuildConfig
 import org.maplibre.android.camera.CameraPosition
 import org.maplibre.android.geometry.LatLng
+import org.maplibre.android.maps.MapLibreMap
 import org.maplibre.android.maps.MapLibreMapOptions
 import org.maplibre.android.maps.MapView
+import org.maplibre.android.maps.Style
+import org.maplibre.android.style.layers.FillLayer
+import org.maplibre.android.style.layers.PropertyFactory.fillColor
+import org.maplibre.android.style.layers.PropertyFactory.fillOpacity
+import org.maplibre.android.style.sources.GeoJsonSource
+import org.maplibre.geojson.Feature
+import org.maplibre.geojson.Point
+import org.maplibre.geojson.Polygon
 
-@Composable
-fun CustomMap(modifier: Modifier, styleUrl: String, lat: Double, lon: Double, appContext: Context){
-    val mapOptions = MapLibreMapOptions.createFromAttributes(appContext)
-    mapOptions.apply {
-        apiBaseUri(BuildConfig.MAPTILER_API_PATH)
-        camera(
-            CameraPosition.Builder()
-                .bearing(0.0)
-                .target(LatLng(lat,lon))
-                .zoom(3.9)
-                .build()
-        )
-        maxZoomPreference(26.0)
-        minZoomPreference(2.0)
-        zoomGesturesEnabled(true)
-        scrollGesturesEnabled(true)
-        rotateGesturesEnabled(true)
-        compassEnabled(true)
-        tiltGesturesEnabled(false)
+class PlayMap {
+    private val polygonMapSrcName = "playarea-src"
+    private val polygonFillName = "playarea-fill"
+
+    private lateinit var mapLibreMap: MapLibreMap
+    private var polygonPoints = mutableListOf<Point>()
+
+    /*fun addPolyToMap(positions: Array<LatLng>) {
+        var poly = Polygon.fromLngLats(latLngToPointList(positions))
+
+        val geoSrc = GeoJsonSource("playarea-src", poly)
+        mapLibreMap.getStyle { style ->
+            if (style.removeSource(geoSrc))
+                Log.v("MAP-POLY", "Removed old poly src");
+            style.addSource(geoSrc)
+            val fill = FillLayer("playarea-fill", "playarea-src")
+                .withProperties(
+                    fillColor("#22ff00"),
+                    fillOpacity(0.5f)
+                )
+            style.addLayer(fill)
+        }
+    }*/
+
+    fun addPolyPoint(coord: LatLng): Boolean {
+        var polyFull = false
+        if(polygonPoints.isNotEmpty()) {
+            polygonPoints.removeAt(polygonPoints.lastIndexOf(polygonPoints.first()))
+        }
+        polygonPoints.add(Point.fromLngLat(coord.longitude, coord.latitude))
+        mapLibreMap.getStyle{ style -> polyFull = updatePolygon(style) }
+        return polyFull
     }
-    val mapView = MapView(appContext, mapOptions)
 
-    AndroidView(
-        modifier = modifier,
-        factory = { mapView }) { view ->
-                view.getMapAsync { map ->
-                    map.setStyle(styleUrl)
+    fun updatePolygon(style: Style): Boolean {
+        polygonPoints.add(polygonPoints.first()) //finishes polygon to full circle
+        if (polygonPoints.size < 3) {
+            // polygon to small
+            Log.w("Polygon Making", "updatePolygon size: " + polygonPoints.size)
+            return false;
+        }
+
+        val source = style.getSourceAs<GeoJsonSource>(polygonMapSrcName) ?: return false;
+        val polygon = Polygon.fromLngLats(listOf(polygonPoints))
+        source.setGeoJson(Feature.fromGeometry(polygon))
+        return true
+    }
+
+    fun getMapLibreMap(): MapLibreMap {
+        return this.mapLibreMap;
+    }
+
+    fun latLngToPointList(positions: Array<LatLng>): List<List<Point>> {
+        val points = positions.map {
+            Point.fromLngLat(it.longitude, it.latitude)
+        }.toMutableList()
+
+        if (points.first() != points.last()) {
+            points.add(points.first())
+        }
+        return listOf(points)
+    }
+
+    @Composable
+    fun CustomMap(
+        modifier: Modifier, lat: Double, lon: Double, appContext: Context,
+        onMapReady: (PlayMap) -> Unit
+    ) {
+        val mapOptions = MapLibreMapOptions.createFromAttributes(appContext)
+        mapOptions.apply {
+            apiBaseUri(BuildConfig.MAPTILER_API_PATH)
+            camera(
+                CameraPosition.Builder()
+                    .bearing(0.0)
+                    .target(LatLng(lat, lon))
+                    .zoom(12.0)
+                    .build()
+            )
+            maxZoomPreference(26.0)
+            minZoomPreference(2.0)
+            zoomGesturesEnabled(true)
+            scrollGesturesEnabled(true)
+            rotateGesturesEnabled(true)
+            compassEnabled(true)
+            tiltGesturesEnabled(false)
+        }
+        val mapView = MapView(appContext, mapOptions)
+        AndroidView(
+            modifier = modifier, factory = { mapView }) { view ->
+            view.getMapAsync { map ->
+                mapLibreMap = map
+                mapLibreMap.setStyle(BuildConfig.MAPTILER_API_PATH){ style ->
+                    //sets style of polygon fill and links list of points
+                    val poly = Polygon.fromLngLats(listOf(polygonPoints))
+                    val geoSrc = GeoJsonSource(polygonMapSrcName, poly)
+                    style.addSource(geoSrc)
+                    val fill = FillLayer(polygonFillName, polygonMapSrcName)
+                        .withProperties(
+                            fillColor("#22ff00"),
+                            fillOpacity(0.5f)
+                        )
+                    style.addLayer(fill)
                 }
+                onMapReady(this)
             }
+        }
+    }
 }
 
-/*
-@Composable
-fun rememberMapViewWithLifecycle(): MapView { //saves resources. generated by chatgpt
-    val context = LocalContext.current
-    val mapView = remember { MapView(context) }
 
-    val lifecycle = LocalLifecycleOwner.current.lifecycle
 
-    DisposableEffect(lifecycle) {
-        val observer = LifecycleEventObserver { _, event ->
-            when (event) {
-                Lifecycle.Event.ON_CREATE -> mapView.onCreate(null)
-                Lifecycle.Event.ON_START -> mapView.onStart()
-                Lifecycle.Event.ON_RESUME -> mapView.onResume()
-                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
-                Lifecycle.Event.ON_STOP -> mapView.onStop()
-                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
-                else -> Unit
-            }
-        }
-
-        lifecycle.addObserver(observer)
-        onDispose {
-            lifecycle.removeObserver(observer)
-        }
-    }
-
-    return mapView
-}*/
